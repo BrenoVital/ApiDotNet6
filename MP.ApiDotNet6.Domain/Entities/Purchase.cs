0ba042045dba0c0c7dfd9df5c68c50241ada8694
Breno Vital <brenovital50@gmail.com>
2024-02-14 18:03:46 -0300

    Iniciando

--- MP.ApiDotNet6.Domain/Entities/Purchase.cs
+++ MP.ApiDotNet6.Domain/Entities/Purchase.cs
@@ -12,7 +12,6 @@ namespace MP.ApiDotNet6.Domain.Entities
         public DateTime Date { get; private set; }
         public Person Person { get; set; }
         public Product Product { get; set; }
-
         public Purchase(int productId, int personId, DateTime? date)
         {
             Validation(productId, personId, date);
@@ -27,7 +26,7 @@ namespace MP.ApiDotNet6.Domain.Entities
 
         private void Validation(int productId, int personId, DateTime? date)
         {
-          
+
             DomainValidationException.When(productId < 0, "Id do produto deve ser informado.");
             DomainValidationException.When(personId < 0, "Id da pessoa deve ser informado.");
             DomainValidationException.When(!date.HasValue, "Data da compra deve ser informada.");
