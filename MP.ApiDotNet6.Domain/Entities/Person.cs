952bbd8ced46afd08ae80c1d5780c461f8d054bd
Breno Vital <brenovital50@gmail.com>
2024-02-14 18:02:53 -0300

    Adicionar arquivos de projeto.

---
+++ MP.ApiDotNet6.Domain/Entities/Person.cs
@@ -0,0 +1,40 @@
+﻿
+using MP.ApiDotNet6.Domain.Validations;
+
+namespace MP.ApiDotNet6.Domain.Entities
+
+{
+    public sealed class Person
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Document { get; private set; }
+        public string Phone { get; private set; }
+        public ICollection<Purchase> Purchases { get; set; }
+        public Person(string name, string document, string phone)
+        {
+            Validation(document, name, phone);
+        }
+
+        public Person(int id, string name, string document, string phone)
+        {
+            DomainValidationException.When(id < 0, "Id inválido");
+            Id = id;
+            Validation(document, name, phone);
+        }
+
+        private void Validation(string document, string name, string phone)
+        {
+            DomainValidationException.When(string.IsNullOrEmpty(name), "Nome é obrigatório");
+            DomainValidationException.When(name.Length < 3, "O campo nome precisa ter no mínimo 3 caracteres");
+            DomainValidationException.When(string.IsNullOrEmpty(document), "Documento é obrigatório");
+            DomainValidationException.When(document.Length < 11, "O campo documento precisa ter no mínimo 11 caracteres");
+            DomainValidationException.When(string.IsNullOrEmpty(phone), "Telefone é obrigatório");
+            DomainValidationException.When(phone.Length < 8, "O campo telefone precisa ter no mínimo 9 caracteres");
+
+            Name = name;
+            Document = document;
+            Phone = phone;
+        }
+    }
+}
---
+++ MP.ApiDotNet6.Domain/Entities/Product.cs
@@ -0,0 +1,39 @@
+﻿
+using MP.ApiDotNet6.Domain.Validations;
+using System.Numerics;
+
+namespace MP.ApiDotNet6.Domain.Entities
+
+{
+    public sealed class Product
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string CodErp { get; private set; }
+        public decimal Price { get; private set; }
+        public ICollection<Purchase> Purchases { get; set; }
+        public Product(string name, string codErp, decimal price)
+        {
+            Validation(name, codErp, price);
+        }
+
+        public Product(int id, string name, string codErp, decimal price)
+        {
+            DomainValidationException.When(id < 0, "Id do produto deve ser informado.");
+            Id = id;
+            Validation(codErp, name, price);
+        }
+
+        private void Validation(string name, string codErp, decimal price)
+        {
+            DomainValidationException.When(string.IsNullOrEmpty(name), "Nome é obrigatório");
+            DomainValidationException.When(name.Length < 3, "O campo nome precisa ter no mínimo 3 caracteres");
+            DomainValidationException.When(string.IsNullOrEmpty(codErp), "Código ERP é obrigatório");
+            DomainValidationException.When(price < 0, "Preço é obrigatório");
+
+            Name = name;
+            CodErp = codErp;
+            Price = price;
+        }
+    }
+}
